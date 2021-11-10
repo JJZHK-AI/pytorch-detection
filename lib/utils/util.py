e2334665964fbcb98038504955b3ad8a4d8555de
JJZHK <jeffcobile@gmail.com>
2021-11-10 19:59:32 +0800

    👍 修改yolov1

--- lib/utils/util.py
+++ lib/utils/util.py
@@ -21,6 +21,10 @@ def write_voc_results_file(cfg, output_dir, all_boxes, infos):
                     continue
                 # the VOCdevkit expects 1-based indices
                 for k in range(dets.shape[0]):
+                    print('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
+                            format(info[0]['img_id'], dets[k, -1],
+                                   dets[k, 0] + 1, dets[k, 1] + 1,
+                                   dets[k, 2] + 1, dets[k, 3] + 1))
                     f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
                             format(info[0]['img_id'], dets[k, -1],
                                    dets[k, 0] + 1, dets[k, 1] + 1,
@@ -54,7 +58,7 @@ def do_python_eval(cfg, infos, output_dir, use_07=True):
 def voc_eval(detpath, infos, classname, ovthresh=0.5, use_07_metric=True):
     recs = {}
     for i, info in enumerate(infos):
-        imagename = info['img_id']
+        imagename = info[0]['img_id']
         recs[imagename] = info['detail']
 
     # extract gt objects for this class
