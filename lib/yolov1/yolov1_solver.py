8994a3a3c9b6f0986cd13cdecae4b672819782ce
JJZHK <jeffcobile@gmail.com>
2021-11-10 19:44:47 +0800

    👍 修改yolov1

--- lib/yolov1/yolov1_solver.py
+++ lib/yolov1/yolov1_solver.py
@@ -36,7 +36,7 @@ class Yolov1Solver(Solver):
             pass
         else:
             weights_json = torch.load(weights)
-            print(weights)
+            print(weights_json)
             self.model.load_state_dict(weights_json)
 
     def init_test_loader(self):
