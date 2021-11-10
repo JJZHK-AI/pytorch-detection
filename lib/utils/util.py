2b653651a17a66ee0cac319f163d1b4b6986453d
JJZHK <jeffcobile@gmail.com>
2021-11-10 19:55:28 +0800

    👍 修改yolov1

--- lib/utils/util.py
+++ lib/utils/util.py
@@ -22,7 +22,7 @@ def write_voc_results_file(cfg, output_dir, all_boxes, infos):
                 # the VOCdevkit expects 1-based indices
                 for k in range(dets.shape[0]):
                     f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
-                            format(info['img_id'], dets[k, -1],
+                            format(info[0]['img_id'], dets[k, -1],
                                    dets[k, 0] + 1, dets[k, 1] + 1,
                                    dets[k, 2] + 1, dets[k, 3] + 1))
 
