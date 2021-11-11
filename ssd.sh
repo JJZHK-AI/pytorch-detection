d8c1d2cd4f383916619c926a0ab89a95d4c51fd3
JJZHK <jeffcobile@gmail.com>
2021-11-11 11:27:03 +0800

    👍 修改yolov1

--- ssd.sh
+++ ssd.sh
@@ -1,5 +1,5 @@
 #
-python ssd.py -dataroot=/Users/jjzhk/data/ -model=ssd -imgsize=300 -datatype=voc -net=vgg16 -phase=train
+python ssd.py -dataroot=/ -model=ssd -imgsize=300 -datatype=voc -net=vgg16 -phase=eval
 # pip install git+https://gitee.com/jjzhk-tools/pyjjzhktools.git
 # pip install tabulate
 # conda install -c conda-forge pycocotools
